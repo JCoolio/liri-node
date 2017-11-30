51f956126b9a22cd345dc7facef27706854d64b2
Jacob Cooley <TheEnigmaMachine@Jacobs-MacBook-Pro.local>
2017-11-29 21:09:04 -0600

    Final

---
+++ keys.js
@@ -0,0 +1,26 @@
+var twitterKeys = {
+  consumer_key: 'RCbqSLUcWtvQnsRQQhSkWZkVh',
+  consumer_secret: 'sbm1LjI2tqLuJuyN8kFy95I4t0XHvuiukwKNbso06Ybm4duB08',
+  access_token_key: '706875353403252737-0ULxwqzr1nyh0pDgHIwjonV0KivQzOw',
+  access_token_secret: 'xZJ6lE0cllsfMmoQdvFm8ozBnpWOwQhTcIvUJau7xenB9',
+}
+
+
+var spotifyKeys = {
+	id: "6a00eb6f5e88458c956af803990ad918",
+	secret: "43d7a4933e9647bbbc597a314acebe84"
+}
+
+
+var omdbKey = {
+	api_key: "40e9cece"
+}
+
+
+module.exports = {
+
+	twitter: twitterKeys,
+	spotify: spotifyKeys,
+	omdb: omdbKey
+
+}
---
+++ liri.js
@@ -0,0 +1,114 @@
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require("fs");
+
+var twitterKeys = new Twitter(keys.twitter);
+var Spotify = new Spotify(keys.spotify);
+var Omdb = keys.omdb.api_key
+
+var userInput1 = process.argv[2];
+var userInput2 = process.argv[3];
+
+
+if (userInput1 === "my-tweets") {
+		myTweets();
+		logging();
+}
+
+if (userInput1 === "spotify-this-song") {
+
+	if (userInput2 === undefined) {
+		spotifyThis("Ballin");
+		logging();
+	}
+	else {
+		spotifyThis(userInput2);
+		logging();
+	}
+}
+
+if (userInput1 === "movie-this") {
+
+	if (userInput2 === undefined) {
+		movieThis("dr+strange");
+		logging();
+	}
+
+	else {
+		movieThis(userInput2);
+		logging();
+	}
+}
+
+if (userInput1 === "do-what-it-says") {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+		if (error) {
+    		return console.log(error);
+  		}
+
+  		var dataArr = data.split(", ");
+
+  		for (var i = 0; i < dataArr.length; i++) {
+  		if (dataArr[i] === "spotify-this-song") {
+  				i++;
+  				spotifyThis(dataArr[i]);
+  		}
+  		if (dataArr[i] === "movie-this") {
+  			i++;
+  			movieThis(dataArr[i]);
+  		}
+  		if (dataArr[i] === "my-tweets") {
+  			myTweets();
+  		}
+  	}
+	})
+	logging();
+}
+function movieThis (input) {
+	var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=" + Omdb;
+		request(queryUrl, function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				console.log("Title: " + JSON.parse(body).Title);
+            	console.log("Year: " + JSON.parse(body).Year);
+            	console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+            	console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+            	console.log("Country: " + JSON.parse(body).Country);
+            	console.log("Language: " + JSON.parse(body).Language)
+            	console.log("Plot: " + JSON.parse(body).Plot);
+            	console.log("Actors: " + JSON.parse(body).Actors);
+			}
+		})
+}
+function spotifyThis (input) {
+	Spotify.search({ type: 'track', query: input }, function(err, data) {
+		if (err) {
+		return console.log('Error occurred: ' + err);
+		}
+		console.log(data.tracks.items[0].album.artists[0].name);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].preview_url);
+
+
+	});
+
+}
+function myTweets () {
+
+		var params = {screen_name: 'HTownProgram'};
+	   twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+
+	  	for (var i = 0; i < tweets.length; i++) {
+	  		console.log("Tweet: '" + tweets[i].text + "' created on " + tweets[i].created_at )
+	  	}
+
+
+	  }
+	});
+
+}
+}
